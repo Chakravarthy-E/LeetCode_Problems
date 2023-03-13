275502a5d7802d62cb093a47c5b997d5429afb2f
Chakravarthy-7 <chakravarthy.8328@gmai.com>
2023-03-13 11:34:46 +0530

    first Commit

---
+++ TwoSum.js
@@ -0,0 +1,17 @@
+/*
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*/
+let TwoSum = (nums, target) => {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; i++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+};
+let nums = [2, 2, 12, 20];
+let target = 14;
+console.log(TwoSum(nums, target));
