268b35fffea51359aeedb1219a5c65a5cf30deac
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-09-09 10:33:48 +0530

    9/9/23

---
+++ removeDuplicates.js
@@ -0,0 +1,10 @@
+var removeDuplicates = function (nums) {
+  let ans = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[i - 1]) {
+      nums[ans] = nums[i];
+      ++ans;
+    }
+  }
+  return ans;
+};
