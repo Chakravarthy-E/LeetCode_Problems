e7d2cb6d0d38b924ab9211232703ffe7872d897b
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-09-10 20:37:20 +0530

    10/9/2023

--- buysellstock.js
+++ buysellstock.js
@@ -1,37 +1,43 @@
-let fs = require("fs");
-let data = fs.readFileSync(0, "utf-8");
-let idx = 0;
-data = data.split("\n");
-function readLine() {
-  idx++;
-  return data[idx - 1].trim();
-}
-// let BuySellStock = (arr, n) => {
-//   let min = Infinity;
-//   let max = 0;
-//   for (let i = 0; i < n; i++) {
-//     if (arr[i] < min) {
-//       min = arr[i];
-//     } else if (arr[i] - min > max) {
-//       max = arr[i] - min;
-//     }
-//   }
-//   return max;
-// };
-// let arr = readLine().split(" ").map(Number);
-// let n = arr.length;
-// console.log(BuySellStock(arr, n));
+/**
+ * 
+ *121. Best Time to Buy and Sell Stock
+Easy
+28.3K
+948
+Companies
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+*/
 const maxProfit = (prices) => {
-    let left = 0; 
-    let max_profit = 0;
-    for ( let right = 1;right < prices.length ; right++) {
-      if (prices[left] < prices[right]) {
-        let profit = prices[right] - prices[left]; // our current profit
-  
-        max_profit = Math.max(max_profit, profit);
-      } else {
-        left = right;
-      }
+  let min = Infinity;
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < min) {
+      min = prices[i];
+    } else if (prices[i] - min > profit) {
+      profit = prices[i] - min;
     }
-    return max_profit;
-  };
+  }
+  return profit;
+};
+const prices = [7, 1, 5, 3, 6, 4];
+console.log(maxProfit(prices));
