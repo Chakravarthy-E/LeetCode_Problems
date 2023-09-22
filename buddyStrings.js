cac18afa151bf959e048b3f8c2fb4558af1de603
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-09-22 21:28:32 +0530

    22/9/23

---
+++ buddyStrings.js
@@ -0,0 +1,53 @@
+/**
+ * 859. Buddy Strings
+Easy
+Topics
+Companies
+Given two strings s and goal, return true if you can swap two letters in s so the result is equal to goal, otherwise, return false.
+
+Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
+
+For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
+ 
+
+Example 1:
+
+Input: s = "ab", goal = "ba"
+Output: true
+Explanation: You can swap s[0] = 'a' and s[1] = 'b' to get "ba", which is equal to goal.
+Example 2:
+
+Input: s = "ab", goal = "ab"
+Output: false
+Explanation: The only letters you can swap are s[0] = 'a' and s[1] = 'b', which results in "ba" != goal.
+Example 3:
+
+Input: s = "aa", goal = "aa"
+Output: true
+Explanation: You can swap s[0] = 'a' and s[1] = 'a' to get "aa", which is equal to goal.
+ */
+const buddyStrings = (s, goal) => {
+  if (s.length !== goal.length || s.length === 1 || goal.length === 1) {
+    return false;
+  }
+  if (s === goal) {
+    const data = new Set(s);
+    return data.size < s.length;
+  }
+
+  const diff = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== goal[i]) {
+      diff.push(i);
+    }
+  }
+
+  return (
+    diff.length === 2 &&
+    s[diff[0]] === goal[diff[1]] &&
+    s[diff[1]] === goal[diff[0]]
+  );
+};
+const s = "ab",
+  goal = "ba";
+console.log(buddyStrings(s, goal));
