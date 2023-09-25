8ad27b5c97d987ed2a816a4c81cfc13056751119
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-09-25 20:56:01 +0530

    25/9/23

---
+++ largestSum.js
@@ -0,0 +1,43 @@
+/**
+ * 1005. Maximize Sum Of Array After K Negations
+Easy
+Topics
+Companies
+Given an integer array nums and an integer k, modify the array in the following way:
+
+choose an index i and replace nums[i] with -nums[i].
+You should apply this process exactly k times. You may choose the same index i multiple times.
+
+Return the largest possible sum of the array after modifying it in this way.
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,3], k = 1
+Output: 5
+Explanation: Choose index 1 and nums becomes [4,-2,3].
+Example 2:
+
+Input: nums = [3,-1,0,2], k = 3
+Output: 6
+Explanation: Choose indices (1, 2, 2) and nums becomes [3,1,0,2].
+Example 3:
+
+Input: nums = [2,-3,-1,5,-4], k = 2
+Output: 13
+Explanation: Choose indices (1, 4) and nums becomes [2,3,-1,5,4].
+ */
+const largestSumAfterKNegations = (nums, k) => {
+  let i = 0;
+  while (i < k) {
+    nums.sort((a, b) => a - b);
+    nums[0] = -nums[0];
+    i++;
+  }
+  return nums.reduce((acc, cur) => acc + cur, 0);
+
+};
+const nums = [4, 2, 3],
+  k = 1;
+console.log(largestSumAfterKNegations(nums, k));
