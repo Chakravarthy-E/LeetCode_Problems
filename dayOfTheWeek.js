8e64d31a354a4a2cadb3ba8e7b38eace2f8bbe02
Chakravarthy-7 <chakravarthy.8328@gmail.com>
2023-10-01 21:00:47 +0530

    1/10/23

---
+++ dayOfTheWeek.js
@@ -0,0 +1,36 @@
+/**
+ * 1185. Day of the Week
+Easy
+Topics
+Companies
+Hint
+Given a date, return the corresponding day of the week for that date.
+
+The input is given as three integers representing the day, month and year respectively.
+
+Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+
+ 
+
+Example 1:
+
+Input: day = 31, month = 8, year = 2019
+Output: "Saturday"
+Example 2:
+
+Input: day = 18, month = 7, year = 1999
+Output: "Sunday"
+Example 3:
+
+Input: day = 15, month = 8, year = 1993
+Output: "Sunday"
+ */
+const dayOfTheWeek = (day, month, year) => {
+  let options = { weekday: "long" };
+  let date = new Date(year, month - 1, day);
+  return date.toLocaleDateString("en-US", options);
+};
+const day = 31,
+  month = 8,
+  year = 2019;
+console.log(dayOfTheWeek(day, month, year));
